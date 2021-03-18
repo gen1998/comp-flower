6140761e28f907b462240d9ecc5628179360255c
Gen Takagi <hamham37337@gmail.com>
2021-03-18 20:16:06 +0900

    first train

---
+++ run.py
@@ -0,0 +1,82 @@
+import os
+import argparse
+import json
+import pandas as pd
+import os
+import torch
+from torch import nn
+
+from sklearn.model_selection import StratifiedKFold
+
+from src.augmentation import *
+from src.dataset import *
+from src.utils import *
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./config/default.json')
+
+    options = parser.parse_args()
+    config = json.load(open(options.config))
+
+    # train 用 df の作成
+    train_df = pd.DataFrame()
+    base_train_data_path = '../flowers-recognition/train/'
+
+    train_data_labels = ['daisy',
+                        'dandelion',
+                        'rose',
+                        'sunflower',
+                        'tulip']
+
+    for one_label in train_data_labels:
+        one_label_df = pd.DataFrame()
+        one_label_paths = os.path.join(base_train_data_path, one_label)
+        one_label_df['image_path'] = [os.path.join(one_label_paths, f) for f in os.listdir(one_label_paths)]
+        one_label_df['label'] = one_label
+        train_df = pd.concat([train_df, one_label_df])
+    train_df = train_df.reset_index(drop=True)
+
+    # train の label を数字にエンコードする
+
+    label_dic = {"daisy":0, "dandelion":1, "rose":2,"sunflower":3, "tulip":4}
+    train_df["label"]=train_df["label"].map(label_dic)
+
+    # train
+    train = train_df
+    seed_everything(config['seed'])
+
+    folds = StratifiedKFold(n_splits=config['fold_num'], shuffle=True, random_state=config['seed']).split(np.arange(train.shape[0]), train.label.values)    
+    for fold, (trn_idx, val_idx) in enumerate(folds):
+
+        if fold > 0: # 時間がかかるので最初のモデルのみ
+            break
+
+        print(f'Training with fold {fold} started (train:{len(trn_idx)}, val:{len(val_idx)})')
+
+        train_loader, val_loader = prepare_dataloader(train, (config["img_size_h"], config["img_size_w"]), trn_idx, val_idx, train_bs=config["train_bs"], valid_bs=config["valid_bs"], num_workers=config["num_workers"] )
+
+        device = torch.device(config['device'])
+
+        model = FlowerImgClassifier(config['model_arch'], train.label.nunique(), pretrained=True).to(device)
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=config['T_0'], T_mult=1, eta_min=config['min_lr'], last_epoch=-1)
+
+        loss_tr = nn.CrossEntropyLoss().to(device)
+        loss_fn = nn.CrossEntropyLoss().to(device)
+
+        for epoch in range(config['epochs']):
+            train_one_epoch(epoch, model, loss_tr, optimizer, train_loader, device, config['verbose_step'],scheduler=scheduler, schd_batch_update=False)
+
+            with torch.no_grad():
+                valid_one_epoch(epoch, model, loss_fn, val_loader, device, config['verbose_step'], scheduler=None, schd_loss_update=False)
+
+            torch.save(model.state_dict(),f'save/{config["model_arch"]}_fold_{fold}_{epoch}')
+
+        del model, optimizer, train_loader, val_loader,  scheduler
+        torch.cuda.empty_cache()
+        print("\n")
+
+if __name__ == '__main__':
+    main()
---
+++ src/dataset.py
@@ -0,0 +1,180 @@
+
+import time
+from tqdm import tqdm
+
+import numpy as np
+import torch
+from torch import nn
+from torch.cuda.amp import autocast, GradScaler
+from torch.utils.data import Dataset
+
+import timm
+
+from src.augmentation import *
+from src.utils import *
+
+class FlowerDataset(Dataset):
+    def __init__(self, df,
+                 shape, # 追加
+                 transforms=None,
+                 output_label=True,
+                 one_hot_label=False,
+                 image_name_col = "image_path",
+                 label_col = "label"
+                ):
+
+        super().__init__()
+        self.shape = shape
+        self.df = df.reset_index(drop=True).copy()
+        self.transforms = transforms
+        self.output_label = output_label
+        self.one_hot_label = one_hot_label
+        self.image_name_col = image_name_col
+        self.label_col = label_col
+
+        if output_label == True:
+            self.labels = self.df[self.label_col].values
+            if one_hot_label is True:
+                self.labels = np.eye(self.df[self.label_col].max()+1)[self.labels]
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, index: int):
+
+        if self.output_label:
+            target = self.labels[index]
+
+        img  = get_img(self.df.loc[index][self.image_name_col])
+
+        if self.transforms:
+            img = self.transforms(image=img)['image']
+
+        if self.output_label == True:
+            return img, target
+        else:
+            return img
+
+class FlowerImgClassifier(nn.Module):
+    def __init__(self, model_arch, n_class, pretrained=True):
+        super().__init__()
+        self.model = timm.create_model(model_arch, pretrained=pretrained)
+        n_features = self.model.classifier.in_features
+        self.model.classifier = nn.Linear(n_features, n_class)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+def prepare_dataloader(df, input_shape, trn_idx, val_idx, train_bs, valid_bs, num_workers):
+    train_ = df.loc[trn_idx,:].reset_index(drop=True)
+    valid_ = df.loc[val_idx,:].reset_index(drop=True)
+
+    train_ds = FlowerDataset(train_, input_shape, transforms=get_train_transforms(input_shape), output_label=True, one_hot_label=False)
+    valid_ds = FlowerDataset(valid_, input_shape, transforms=get_valid_transforms(input_shape), output_label=True)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_ds,
+        batch_size=train_bs,
+        pin_memory=True, # faster and use memory
+        drop_last=False,
+        shuffle=True,
+        num_workers=num_workers,
+    )
+    val_loader = torch.utils.data.DataLoader(
+        valid_ds,
+        batch_size=valid_bs,
+        num_workers=num_workers,
+        shuffle=False,
+        pin_memory=True,
+    )
+    return train_loader, val_loader
+
+def train_one_epoch(epoch, model, loss_fn, optimizer, train_loader, device, verbose_step, scheduler=None, schd_batch_update=False):
+    model.train()
+    scaler = GradScaler()
+
+    t = time.time()
+    running_loss = None
+
+    pbar = tqdm(enumerate(train_loader), total=len(train_loader))
+    for step, (imgs, image_labels) in pbar:
+        imgs = imgs.to(device).float()
+        image_labels = image_labels.to(device).long()
+
+        with autocast():
+            image_preds = model(imgs)
+            loss = loss_fn(image_preds, image_labels)
+
+            scaler.scale(loss).backward()
+
+            if running_loss is None:
+                running_loss = loss.item()
+            else:
+                running_loss = running_loss * .99 + loss.item() * .01
+
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
+            if scheduler is not None and schd_batch_update:
+                scheduler.step()
+
+            if ((step + 1) % verbose_step == 0) or ((step + 1) == len(train_loader)):
+                description = f'epoch {epoch} loss: {running_loss:.4f}'
+                pbar.set_description(description)
+
+    print("train: "+ description)
+    if scheduler is not None and not schd_batch_update:
+        scheduler.step()
+
+def valid_one_epoch(epoch, model, loss_fn, val_loader, device, verbose_step, scheduler=None, schd_loss_update=False):
+    model.eval()
+
+    t = time.time()
+    loss_sum = 0
+    sample_num = 0
+    image_preds_all = []
+    image_targets_all = []
+
+    pbar = tqdm(enumerate(val_loader), total=len(val_loader))
+    for step, (imgs, image_labels) in pbar:
+        imgs = imgs.to(device).float()
+        image_labels = image_labels.to(device).long()
+
+        image_preds = model(imgs)
+        image_preds_all += [torch.argmax(image_preds, 1).detach().cpu().numpy()]
+        image_targets_all += [image_labels.detach().cpu().numpy()]
+
+        loss = loss_fn(image_preds, image_labels)
+
+        loss_sum += loss.item()*image_labels.shape[0]
+        sample_num += image_labels.shape[0]
+
+        if ((step + 1) % verbose_step== 0) or ((step + 1) == len(val_loader)):
+            description = f'epoch {epoch} loss: {loss_sum/sample_num:.4f}'
+            pbar.set_description(description)
+
+    print("valid "+ description)
+    image_preds_all = np.concatenate(image_preds_all)
+    image_targets_all = np.concatenate(image_targets_all)
+    print('validation multi-class accuracy = {:.4f}'.format((image_preds_all==image_targets_all).mean()))
+
+    if scheduler is not None:
+        if schd_loss_update:
+            scheduler.step(loss_sum/sample_num)
+        else:
+            scheduler.step()
+
+
+def inference_one_epoch(model, data_loader, device):
+    model.eval()
+    image_preds_all = []
+    pbar = tqdm(enumerate(data_loader), total=len(data_loader))
+    for step, (imgs) in pbar:
+        imgs = imgs.to(device).float()
+
+        image_preds = model(imgs)   #output = model(input)
+        image_preds_all += [torch.softmax(image_preds, 1).detach().cpu().numpy()]
+
+    image_preds_all = np.concatenate(image_preds_all, axis=0)
+    return image_preds_all
