8728541b955c0f0d535af0f7339383344ddd1352
Gen Takagi <hamham37337@gmail.com>
2021-03-18 21:59:55 +0900

    new

--- test.py
+++ test.py
@@ -14,19 +14,28 @@ class FlowerImgClassifier(nn.Module):
         return x
 
 import torch.nn.functional as F
-class Model(nn.Module):
-  def __init__(self):
-    super(Model,self).__init__()
-    self.fc1 = nn.Linear(10,100)
-    self.fc2 = nn.Linear(100,10)
 
-  def forward(self,x):
-    x = self.fc1(x)
-    x = F.relu(x)
-    x = self.fc2(x)
-    return x
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 if __name__ == '__main__':
     DEVICE = torch.device("cuda:0")
     #model = FlowerImgClassifier("tf_efficientnet_b0", 4, pretrained=True).to(DEVICE)
-    model = Model.to(DEVICE)
+    model = Net()
+    model.to(DEVICE)
