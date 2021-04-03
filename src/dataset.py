7c80e92b77e549c28e74e7efc29a53ed5006d539
Gen Takagi <hamham37337@gmail.com>
2021-04-03 19:18:32 +0900

    new

--- src/dataset.py
+++ src/dataset.py
@@ -235,7 +235,7 @@ def inference_one_epoch_tsne(model, data_loader, device):
     image_preds_all = []
     pbar = tqdm(enumerate(data_loader), total=len(data_loader))
     tmp_model = model
-    tmp_model.conv_head = Identity()
+    tmp_model.conv_hea = Identity()
 
     for step, (imgs) in pbar:
         imgs = imgs.to(device).float()
