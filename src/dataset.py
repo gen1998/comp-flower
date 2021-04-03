a11f67e6bb1a1cb75b4d1824f52861896d400336
Gen Takagi <hamham37337@gmail.com>
2021-04-03 19:31:31 +0900

    new

--- src/dataset.py
+++ src/dataset.py
@@ -249,7 +249,7 @@ def inference_one_epoch_tsne(model, data_loader, device):
         #image_preds = model.model.bn2(imgs)   #output = model(input)
         model.model.bn2.register_forward_hook(get_activation('bn2'))
         image_preds = model(imgs)
-        print(activation["bn2"])
+        print(torch.softmax(activation["bn2"], 1).detach().cpu().numpy().shape)
         #print(torch.softmax(image_preds, 1).detach().cpu().numpy().shape)
         #print(torch.softmax(image_preds, 1).detach().cpu().numpy().shape)
         image_preds_all += [torch.softmax(image_preds, 1).detach().cpu().numpy()]
